46b5664e940d2c83345bbcb125d1e0e4d82e3c42
skwisgaarxskwigelf <sabina.sfn@gmail.com>
2017-11-29 00:58:02 +0300

    sync

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
 /**
  * Created by ratherrum on 28.11.17.
  */
-
 $(document).ready(function () {
-    $('button[type=submit]').click(function (event) {
-        var toPost = {
-            'name': $('input[id=inputName]').val(),
-            'text': $('textarea[id=redex]').val()
-        };
+    function refresh() {
         $.ajax({
-            type: 'POST',
-            url: 'controller.php',
-            data: toPost,
+            type: 'GET',
+            url: 'getPosts.php',
             dataType: 'json',
             success: function (data) {
-                if (data.success_msg) {
-                    $('.res').fadeIn(4000).append(data.success_msg);
-                }
-                else if (data.error_msg) {
-                    $('.res').fadeIn(4000).append(data.error_msg);
+                $("tbody").empty();
+                for (i = 0; i < 5; i++) {
+                    $("tbody").append('<tr><td>' + data[i].name + '</td><td>' + data[i].body + '</td><td>' + data[i].dtime + '</td></tr>');
                 }
             }
         });
-        event.preventDefault();
-    });
-    $.ajax({
-        type: 'GET',
-        url: 'getPosts.php',
-        dataType: 'json',
-        success: function (data) {
-            for (i = 0; i < 6; i++) {
-                // $(".w3-ul").append('<li><span class="w3-margin-right">' + data[i].name + '</span><span class="w3-margin-left">' + data[i].body + '</span></li>');
-                $("tbody").append('<tr><td>' + data[i].name + '</td><td>' + data[i].body + '</td><td>' + data[i].dtime + '</td></tr>');
+    }
+
+    refresh();
+    $('button[type=submit]').click(function (event) {
+        var dataToPost = {
+            retrievedData: {
+                name: document.querySelector("input[id=inputName]").value,
+                text: document.querySelector("textarea[id=redex]").value
+            },
+            postData: function () {
+                $.ajax({
+                    type: 'POST',
+                    url: 'controller.php',
+                    data: this.retrievedData,
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.success_msg) {
+                            refresh();
+                            setTimeout(function () {
+                                alert(data.success_msg);
+                            }, 100);
+                        } else if (data.error_msg) {
+                            alert(data.error_msg);
+                        }
+                    }
+                });
             }
-        }
+        };
+        dataToPost.postData();
+        event.preventDefault();
     });
 });

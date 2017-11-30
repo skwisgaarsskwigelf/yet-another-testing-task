af637e265e8e6bd3b92885f325561e37d9e63b77
skwisgaarxskwigelf <sabina.sfn@gmail.com>
2017-11-30 12:08:52 +0300

    markup chngs

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css?v=1.001">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <body>
 <div class="w3-top">
@@ -23,7 +23,7 @@
                     </div>
                 </div>
                 <div class="control-group">
-                    <label class="control-label" for="inputText">Text</label>
+                    <label class="control-label" for="inputText">Text (max 50 characters)</label>
                     <div class="controls">
                         <textarea id="redex" maxlength="50" rows="5" id="inputText" placeholder="Text" class="form-control"></textarea>
                     </div>
